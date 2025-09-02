cdceae2c99df4259f45705e22a604617a18522ba
chesboy <chesboy933@gmail.com>
2025-09-02 19:42:59 +0300

    КАЛЬКУЛЯТОР ДЛЯ ДЕЛЕНИЯ

---
+++ Kalkulatopdelitel.py
@@ -0,0 +1,7 @@
+print("                                               КАЛЬКУЛЯТОР")
+print("                                       НА ДЕЛЕНИЕ")
+a =float(input("Введите Делимое: "))
+b =float(input("Введите Делитель: "))
+result = a / b
+print("Ответ : ", result)
+
