e2b6c0374677aac2b4e8a7a2e9121bddd38f90ee
chesboy <chesboy933@gmail.com>
2025-09-02 19:41:05 +0300

    КАЛЬКУЛЯТОР ДЛЯ ВЫЧИТАНИЯ

---
+++ kalkulatormin.py
@@ -0,0 +1,6 @@
+print("                                               КАЛЬКУЛЯТОР")
+print("                                       НА ВЫЧИТАНИЕ")
+a =float(input("Введите уменьшаемое: "))
+b =float(input("Введите вычитаемое: "))
+result = a - b
+print("ответ : ", result)
