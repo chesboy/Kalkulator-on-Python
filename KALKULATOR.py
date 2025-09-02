1bfc84752b291089fa3205fa6e8fb766c7c479d4
chesboy <chesboy933@gmail.com>
2025-09-02 19:39:09 +0300

    КАЛЬКУЛЯТОР НА СЛОЖЕНИЕ

---
+++ KALKULATOR.py
@@ -0,0 +1,7 @@
+print("                                               КАЛЬКУЛЯТОР")
+print("                                       НА СЛОЖЕНИЕ")
+a =float(input("Введите 1 слогаемое: "))
+b =float(input("Введите 2 слогаемое: "))
+result = a + b
+print("Сложенное : ", result)
+
